56edf216c0da0c6a44117b24ccfc5ddcda317724
Darshan Gawande <gawanded24@gmail.com>
2023-06-08 07:55:23 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   let puppies = [
+    {
+      name: 'chaow-chaow',
+      breed: "Golden Retrival",
+      Description: "Looking cute at the begining and sweet with behaviour and very harmless",
+      image: "https://photos.app.goo.gl/3d4o5rgEtKzVfAzg8."
+    },
+    {
+      name: "Tommy",
+      breed: "Golden Retrival",
+      Description: "Looking cute at the begining and moderate harmful and behave like no-one is there",
+      image: "https://photos.google.com/photo/AF1QipPr2DoJgmQp-lCvv9321DwLaujhLF0XobcYngcg"
+    },
+   ];
+
+    return (
+  <div>
+    <h1>Hello Dogg</h1>
+   <ul>
+    {
+      puppies.map((puppy, index) => (
+       
+          <li key={index}>
+            <h1> {puppy.name} </h1>
+            <img src={puppy.image} alt= {puppy.name} width="200px" height="200px" />
+          </li>
+      ))
+    }
+   </ul>
+  </div>
+  )
 }
 
 export default App;
