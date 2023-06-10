c36166d7bead374c079d6b9420c1e9ff91037b9b
Darshan Gawande <gawanded24@gmail.com>
2023-06-10 21:41:52 +0530

    ImageUpload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactDOM } from "react";
 import './App.css';
 
 function App() {
@@ -8,14 +9,14 @@ function App() {
       name: 'chaow-chaow',
       breed: "Golden Retrival",
       Description: "Looking cute at the begining and sweet with behaviour and very harmless",
-      image: "https://photos.app.goo.gl/3d4o5rgEtKzVfAzg8."
+      image: "https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=8"
     },
     {
       name: "Tommy",
       breed: "Golden Retrival",
       Description: "Looking cute at the begining and moderate harmful and behave like no-one is there",
-      image: "https://photos.google.com/photo/AF1QipPr2DoJgmQp-lCvv9321DwLaujhLF0XobcYngcg"
-    },
+      image: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+    }
    ];
 
     return (
@@ -27,7 +28,7 @@ function App() {
        
           <li key={index}>
             <h1> {puppy.name} </h1>
-            <img src={puppy.image} alt= {puppy.name} width="200px" height="200px" />
+            <img src={puppy.image} alt= {puppy.name} width={"200px"} height={"200px"}  />
           </li>
       ))
     }
